0ed78f63a97428f7566174affa4e1084739a2b9e
Ivan Ivankiv <ivan.ivankiv27@gmail.com>
2016-09-14 09:22:04 +0300

    Init commit. Added <app-bar> Component to display header of the app on the page

---
+++ src/app/containers/main.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AppBar } from '../ui';
+
+@Component({
+    selector: 'main-container',
+    directives: [ AppBar ],
+    template: `
+       <div>
+            <app-bar></app-bar>
+            <main class="main">
+                main content will go here
+            </main>
+        </div>
+    `
+})
+
+export class Main {}
